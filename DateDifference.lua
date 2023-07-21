9f84af6e3e484bb7b39f9591f27c43d60430c715
e-labInnovations <mohammedashad23@gmail.com>
2023-07-21 21:08:31 +0530

    upadet function added

--- DateDifference.lua
+++ DateDifference.lua
@@ -1,4 +1,5 @@
 function Initialize()
+    -- Code to initialize the variables and calculate the initial date difference
     local year = tonumber(SKIN:GetVariable('Year'))
     local month = tonumber(SKIN:GetVariable('Month'))
     local day = tonumber(SKIN:GetVariable('Day'))
@@ -13,7 +14,24 @@ function Initialize()
 
     local dateDifference = math.floor((currentDateSeconds - otherDateSeconds) / 86400)
 
-    -- print(dateDifference) -- Uncomment this line to log the date difference during debugging
+    SKIN:Bang('!SetVariable', 'DayCount', dateDifference)
+end
+
+function Update()
+    -- Code to update the DayCount variable on each update
+    local year = tonumber(SKIN:GetVariable('Year'))
+    local month = tonumber(SKIN:GetVariable('Month'))
+    local day = tonumber(SKIN:GetVariable('Day'))
+
+    local currentDate = os.date('*t')
+    local currentDateTable = {year=currentDate.year, month=currentDate.month, day=currentDate.day}
+
+    local otherDateTable = {year=year, month=month, day=day}
+
+    local currentDateSeconds = os.time(currentDateTable)
+    local otherDateSeconds = os.time(otherDateTable)
+
+    local dateDifference = math.floor((currentDateSeconds - otherDateSeconds) / 86400)
 
     SKIN:Bang('!SetVariable', 'DayCount', dateDifference)
 end
