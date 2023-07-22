8893fbaf381b2f6f648833a5352650f65d05c48c
e-labInnovations <mohammedashad23@gmail.com>
2023-07-22 21:42:42 +0530

    Cleaning the lua files and functions

--- ElabinsDaysCounter.lua
+++ ElabinsDaysCounter.lua
@@ -1,5 +1,27 @@
 function Initialize()
-    -- SKIN:Bang('!ShowMeter', 'StartDateInputErrorMessage')
+    UpdateCount()
+end
+
+function Update()
+    UpdateCount()
+end
+
+function UpdateCount()
+    local year = tonumber(SKIN:GetVariable('Year'))
+    local month = tonumber(SKIN:GetVariable('Month'))
+    local day = tonumber(SKIN:GetVariable('Day'))
+
+    local currentDate = os.date('*t')
+    local currentDateTable = {year=currentDate.year, month=currentDate.month, day=currentDate.day}
+
+    local otherDateTable = {year=year, month=month, day=day}
+
+    local currentDateSeconds = os.time(currentDateTable)
+    local otherDateSeconds = os.time(otherDateTable)
+
+    local dateDifference = math.floor((currentDateSeconds - otherDateSeconds) / 86400)
+
+    SKIN:Bang('!SetVariable', 'DayCount', dateDifference)
 end
 
 function UpdateStartDate()
